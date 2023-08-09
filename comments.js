7caf18428c668d1fab37b8a8e4ae7454928ea332
SathVaig <141845254+SathVaig@users.noreply.github.com>
2023-08-09 15:01:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,42 @@
-// create web server
-// run server: node comments.js
-// test: http://localhost:3000/comments
-
-// load modules
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-// create server
-http.createServer(function(request, response) {
-    // parse the pathname
-    var pathname = url.parse(request.url).pathname;
-    // print pathname
-    console.log("Request for " + pathname + " received.");
-    // read the requested file content from file system
-    fs.readFile(pathname.substr(1), function(err, data) {
-        if (err) {
-            console.log(err);
-            // HTTP Status: 404 : NOT FOUND
-            // Content Type: text/plain
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        } else {
-            // Page found
-            // HTTP Status: 200 : OK
-            // Content Type: text/plain
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            // write the content of the file to response body
-            response.write(data.toString());
-        }
-        // send the response body
-        response.end();
+// create web server with express
+const express = require('express');
+const app = express();
+// create body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// create mongoose
+const mongoose = require('mongoose');
+// connect mongoose to database
+mongoose.connect('mongodb://localhost/comments', { useNewUrlParser: true });
+// create schema
+const Schema = mongoose.Schema;
+// create schema for comments
+const commentSchema = new Schema({
+    name: String,
+    comment: String
+});
+// create model for comments
+const Comment = mongoose.model('Comment', commentSchema);
+// set view engine to ejs
+app.set('view engine', 'ejs');
+// set static folder
+app.use(express.static('public'));
+// create route for home page
+app.get('/', (req, res) => {
+    Comment.find({}, (err, data) => {
+        if (err) throw err;
+        res.render('index', { comments: data });
     });
-}).listen(3000);
-
-// print message to console
-console.log('Server running at http://localhost:3000/comments');
+});
+// create route for post request
+app.post('/comment', (req, res) => {
+    const newComment = new Comment(req.body);
+    newComment.save((err, data) => {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+// create server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
