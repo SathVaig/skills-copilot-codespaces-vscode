3c211b9e006cb6e157b656b909624dd323a493cc
SathVaig <141845254+SathVaig@users.noreply.github.com>
2023-08-09 14:51:21 +0000

    Copliot second commit

--- member.js
+++ member.js
@@ -1,6 +1,11 @@
-function skillsmember () {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/skills-member.html'
-    };
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsError = document.getElementById("skillsError");
+    if (skills == "") {
+        skillsError.innerHTML = "Please enter your skills";
+        return false;
+    } else {
+        skillsError.innerHTML = "";
+        return true;
+    }
 }
